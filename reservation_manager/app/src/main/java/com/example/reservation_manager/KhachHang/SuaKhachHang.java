c85005ed01ccc7b051bd99243428ea4c1f054c9e
Trung Thanh Luu <thanh.187pm14010@vanlanguni.vn>
2020-12-15 01:02:24 +0700

    Fixing Update Khach Hang (on progress)

--- reservation_manager/app/src/main/java/com/example/reservation_manager/KhachHang/SuaKhachHang.java
+++ reservation_manager/app/src/main/java/com/example/reservation_manager/KhachHang/SuaKhachHang.java
@@ -1,10 +1,13 @@
 package com.example.reservation_manager.KhachHang;
 
+import android.Manifest;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,8 +15,11 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.reservation_manager.FirebaseController;
 import com.example.reservation_manager.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,9 +32,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 public class SuaKhachHang extends AppCompatActivity {
     ImageView avatar;
@@ -36,9 +45,13 @@ public class SuaKhachHang extends AppCompatActivity {
     RadioButton nam,nu;
     Button save;
     private String uid;
-    private String imguri;
     DatabaseReference databaseReference;
     StorageReference storageReference;
+    KhachHang khachHang;
+    FirebaseController controller;
+    Uri imguri;
+    String idavatar;
+    StorageTask uploadTask;
 
     private void AnhXa() {
         avatar = (ImageView) findViewById(R.id.profile_image);
@@ -101,5 +114,96 @@ public class SuaKhachHang extends AppCompatActivity {
             }
         });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(uploadTask != null && uploadTask.isInProgress()){
+                    Toast.makeText(SuaKhachHang.this, "Đang tiến hành upload hình ảnh", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        UpdateKH();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                controller.UpdateData("KhachHang",uid,khachHang);
+                intentToListKH();
+            }
+        });
+
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions, 1);
+                Filechooser();
+            }
+        });
+    }
+
+    private void intentToListKH(){
+        Intent ListKH_intent = new Intent(SuaKhachHang.this,XemDanhSachKhachHang.class);
+        ListKH_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        ListKH_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(ListKH_intent);
+        finish();
+    }
+
+    private void UpdateKH() {
+        Fireuploader();
+        String name = ht.getText().toString();
+        String phone = sdt.getText().toString();
+        String birthday = ns.getText().toString();
+        Boolean sex = true;
+        if (nam.isSelected() == true) {
+            sex = true;
+        } else {
+            sex = false;
+        }
+        int amount = Integer.parseInt(sld.getText().toString());
+        khachHang = new KhachHang(name,phone,birthday,sex,amount,idavatar);
+    }
+
+    private void Fireuploader() {
+        idavatar = "KhachImages/"+removeAccent(ht.getText().toString()).replaceAll("\\s","")+"."+"png";
+        StorageReference ref = storageReference.child(idavatar);
+        uploadTask=ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+    private String removeAccent(String s) {
+        String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(temp).replaceAll("");
+    }
+
+    private void Filechooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!=null && data.getData()!=null){
+            imguri = data.getData();
+            avatar.setImageURI(imguri);
+        }
     }
 }
