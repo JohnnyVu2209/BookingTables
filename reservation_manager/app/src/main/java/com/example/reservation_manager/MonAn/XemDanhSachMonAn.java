3d871a8dd3824d0fe1dd8e0298e1e5b54854a50b
Khon Vu <vu.187pm14039@vanlanguni.vn>
2020-12-30 20:24:43 +0700

    fix Firebase WriteWithAutoIncreaseKey()

--- reservation_manager/app/src/main/java/com/example/reservation_manager/MonAn/XemDanhSachMonAn.java
+++ reservation_manager/app/src/main/java/com/example/reservation_manager/MonAn/XemDanhSachMonAn.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
